b67f4ebc5470451ce4deedfba97e26a411a71cb2
jitheshm <jitheshmjithooz@gmail.com>
2024-01-03 13:45:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Banner from "./Components/Banner/Banner";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Rowpost from "./Components/RowPost/Rowpost";
+import { action, Animation, comedy, Drama, Romance } from "../src/constants/constant"
+
+function App() { 
+  return (
+    <div>
+      <Navbar />
+      <section className="container-fluid">
+        <Banner />
+        <Rowpost title="Get In On the Action" url={action} />
+        <Rowpost title="Drama" url={Drama} />
+        <Rowpost title="Romance" url={Romance} />
+        <Rowpost title="Animation" url={Animation} />
+        <Rowpost title="Comedy" url={comedy} />
+        <Footer />
+      </section> 
+    </div>
+
+  );
+
+
+}
+
+export default App
---
+++ src/Components/RowPost/Rowpost.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import './Rowpost.css'
+import instance from '../../axios'
+import { baseImageURL } from '../../constants/constant'
+
+
+
+function Rowpost({ title, url }) {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    instance.get(url).then((result) => {
+      setMovies(result.data.results);
+      console.log(result.data.results[0]);
+    })
+
+
+  }, [])
+
+
+  return (
+    <section className="row-section">
+      <div className="row-content">
+        <h2 className="row-data-headline">{title}</h2>
+        <div className="row-data">
+          {
+            movies.map((value, index) => {
+              return (
+                <div key={index} className="card col-5 col-md-4 col-xl-3">
+                  <img className="col-12 row-img" src={`${value ? baseImageURL + value.backdrop_path : ""}`} />
+                  <div className="card-body"><span>{value ? value.original_title : ""}
+                  </span></div>
+                </div>
+              )
+
+            })}
+
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Rowpost
